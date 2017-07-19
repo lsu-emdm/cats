4785b7c70012ed9d5d184dadddd97ff369a0ebba
Jesse Allison <jesse@electrotap.com>
2017-07-19 11:26:44 -0500

    Adding regions class

---
+++ UI-Prototypes-06212017/Working Samples/PianoRoll07192017/regions.js
@@ -0,0 +1,45 @@
+
+function Region(pText, startNote, endNote) {
+	this.phraseNumber;
+	this.phraseText = pText ? pText : "Oh say can you see";
+	this.startNote = startNote;
+	this.endNote = endNote;
+}
+
+Region.prototype.notesInRegion = funtion() {
+	var noteArray = [];
+	for(var i=this.startNote;i<this.endNote; i++){
+		noteArray.push(notes[i]);
+	}
+	return noteArray;
+}
+
+Region.prototype.startTime = function(){
+	return notes[this.startNote].time
+}
+
+Region.prototype.updateNotesInRegion(note, action) {
+	if(action == "remove"){
+		if(note <= this.startNote){
+			this.startNote = this.startNote - 1;
+		}
+		if(note <= this.endNote){
+			this.endNote = this.endNote - 1;
+		}
+	} else if (action == "add") {
+		
+	}
+}
+
+
+
+/*
+// To Create
+region1 = new Region("Oh Say can you see", 1 ,6);
+
+// text?
+region1.phraseText
+region1.startTime()
+regionNotes = region1.notesInRegion()
+region1.updateNotesInRegion(110,"remove");
+*/
